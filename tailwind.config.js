adb3f5009de681f1b674c3288e7aeb78623657fa
David <osodavid001@gmail.com>
2023-09-13 13:59:58 +0100

    added other customer properties and imported font awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ export default {
       fontFamily:{
         Roboto: ["Roboto, sans-serif"],
       },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
     },
   },
   plugins: [],
